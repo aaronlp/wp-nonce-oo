2f316643c660897dc47e774413fff3a5029adb14
Aaron Peachey <aaron@aaronpeacheywebdesign.com>
2017-02-26 23:45:47 +0000

    initial commit, PSR-4 composer package setup

---
+++ src/WP_Nonce_OO.php
@@ -0,0 +1,7 @@
+<?php 
+namespace apwd\WP_Nonce_OO;
+ 
+class WP_Nonce_OO {
+ 
+ 
+}
---
+++ tests/WP_Nonce_OOTest.php
@@ -0,0 +1,8 @@
+<?php
+namespace apwd\WP_Nonce_OO;
+use PHPUnit\Framework\TestCase;
+ 
+class WP_Nonce_OOTest extends TestCase {
+ 
+ 
+}
